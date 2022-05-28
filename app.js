92785d4ee8600b36b8f253d457a20686b39744ba
unknown <sazwerling@gmail.com>
2022-05-27 20:44:29 -0500

    geocode function moved to own file in utils folder

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 // const url = 'http://api.weatherstack.com/current?access_key=ea9c083d5d840352627cce481d258e1c&query=Mexico City&units=f'
 
@@ -16,42 +17,9 @@ const request = require('request')
 
 // const url = 'http://api.weatherstack.com/current?access_key=ea9c083d5d840352627cce481d258e1c&query=' + latitude + ',' + longitude + '&units=f';
 // mapbox // access_token=pk.eyJ1Ijoic2xpbWJpZ2d5IiwiYSI6ImNrbXFvcjd2azAwMDgybm50eXJocWR4cWsifQ.VzNz24swKwxCgnHWDoZGsw
-//geocoding
-// user provides address, convert to lat and long
 
-const geo = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Mexico%20City.json?access_token=pk.eyJ1Ijoic2xpbWJpZ2d5IiwiYSI6ImNrbXFvcjd2azAwMDgybm50eXJocWR4cWsifQ.VzNz24swKwxCgnHWDoZGsw&limit=1'
 
-// request({ url: geo, json: true}, (error, response) => {
-//     if(error){
-//         console.log('Cannot connect to geocoding service.')
-//     } else if(response.body.features.length === 0){
-//         console.log('Location does not exist.')
-//     } else {
-//         const lat = response.body.features[0].center[1]
-//         const long = response.body.features[0].center[0]
-//         console.log(lat, long)
-//     }
- 
-// })
-
-const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2xpbWJpZ2d5IiwiYSI6ImNrbXFvcjd2azAwMDgybm50eXJocWR4cWsifQ.VzNz24swKwxCgnHWDoZGsw&limit=1'
-    request({ url: url, json: true}, (error, response) => {
-        if(error){
-            callback("Cannot connect to geocoding service.", undefined)
-        } else if(response.body.features.length === 0){
-            callback("Location does not exist.", undefined)
-        } else {
-            callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                name: response.body.features[0].text
-            })
-        }
-    })
-}
-
-geocode('Los Angeles', (error, data) => {
+geocode('Boston', (error, data) => {
     console.log(`Error:`, error)
     console.log(`Data:`, data)
 })
---
+++ utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2xpbWJpZ2d5IiwiYSI6ImNrbXFvcjd2azAwMDgybm50eXJocWR4cWsifQ.VzNz24swKwxCgnHWDoZGsw&limit=1'
+    request({ url: url, json: true}, (error, response) => {
+        if(error){
+            callback("Cannot connect to geocoding service.", undefined)
+        } else if(response.body.features.length === 0){
+            callback("Location does not exist.", undefined)
+        } else {
+            callback(undefined, {
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
+                name: response.body.features[0].text
+            })
+        }
+    })
+}
+
+
+module.exports = geocode
