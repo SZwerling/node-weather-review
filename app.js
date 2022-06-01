b60d8d84998f20f38b3accd31e9e9b2b10631c83
unknown <sazwerling@gmail.com>
2022-06-01 08:06:08 -0500

    chain callbacks geocode and forecast

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-forecast(24, -74.3893168105238, (error, data) => {
-    console.log('Error:', error);
-    console.log('Data:', data)
-})
 
 geocode('New Jersey', (error, data) => {
     console.log(`Error:`, error)
     console.log(`Data:`, data)
+    forecast(data.latitude, data.longitude, (error, data) => {
+        console.log('Error:', error);
+        console.log('Data:', data)
+    })
+    
 })
