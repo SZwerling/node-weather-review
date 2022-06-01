25fcb186eb08de9888ce65f788ab47613a6b5c2d
unknown <sazwerling@gmail.com>
2022-06-01 09:02:33 -0500

    command line args w weather app

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const location = process.argv[2];
 
-geocode('New Jersey', (error, data) => {
-    console.log(`Error:`, error)
-    console.log(`Data:`, data)
-    forecast(data.latitude, data.longitude, (error, data) => {
-        console.log('Error:', error);
-        console.log('Data:', data)
+if(location){
+    geocode(process.argv[2], (error, data) => {
+        if(error){
+            return console.log(`Error:`, error)
+        }
+        
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if(error){
+                return console.log('Error:', error);
+            }
+            
+            console.log(data.name, forecastData)
+        })
+        
     })
-    
-})
+} else {
+    console.log("Please provide a location.")
+}
+
+
+
