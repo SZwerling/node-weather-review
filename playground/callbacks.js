5832b86272b91dbe6a91325b23961d27dc9afe0a
unknown <sazwerling@gmail.com>
2022-06-05 21:51:04 -0500

    review callbacks

--- playground/callbacks.js
+++ playground/callbacks.js
@@ -45,3 +45,31 @@ add(1, 4, (sum) => {
         console.log(sum) // Should print: 5 
 })
 
+setTimeout(() => {
+    console.log("one second");
+ }, 1000);
+ 
+ const names = ["slim", "floppy", "Yackackaback"];
+ 
+ const shortNames = names.filter((name) => {
+    return name.length <= 4;
+ });
+ 
+ 
+ // With asynchronous functions we use callbacks instead of return.
+ // We can't just 'return' data in function below
+ const geocode = (address, callback) => {
+    setTimeout(() => {
+     const data = {
+         lat: 23,
+         long: 53
+     }
+     callback(data.lat, data.long) //if we call callback with data as first arg
+    }, 2000);                      //that means we're calling the function we pass
+                                   //in with data as the first argument
+ };
+ 
+ geocode('address', (arg, arg2) => {
+     console.log(arg, arg2)
+ })
+ 
